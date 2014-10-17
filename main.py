f5a538205bafe41450b3f0f831a8af24f447993c
Victor Berger <victor.berger@m4x.org>
2014-10-17 16:02:52 +0200

    We're making python here.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#!/usr/bin/ipython
+#!/usr/bin/python
 
 import nltk
 import random
@@ -9,114 +9,112 @@ from random import randint
 
 
 def file_len(f):
-	for i, l in enumerate(f):
-		pass
-		return i + 1
+    for i, l in enumerate(f):
+        pass
+        return i + 1
 
 
 def getRandomProverb():
-	proverbsFile = open('proverbsList','r')
-	proverbsList = proverbsFile.readlines()
-	line = randint(0,len(proverbsList)-1)
-	proverb = proverbsList[line];
-	proverbsFile.close()
-	return proverb[0:-1]
+    proverbsFile = open('proverbsList','r')
+    proverbsList = proverbsFile.readlines()
+    line = randint(0,len(proverbsList)-1)
+    proverb = proverbsList[line];
+    proverbsFile.close()
+    return proverb[0:-1]
 
 def computeModificationRate(initialLProverb,lProverb):
-	diffWords=0
-	for a,b in zip(initialLProverb,lProverb):
-		if a!=b:
-			diffWords = diffWords + 1
-	return float(diffWords)/len(initialLProverb)
+    diffWords=0
+    for a,b in zip(initialLProverb,lProverb):
+        if a!=b:
+            diffWords = diffWords + 1
+    return float(diffWords)/len(initialLProverb)
 
 def getTags(word):
-	l = nltk.pos_tag([word])
-	tags = [ t for (w,t) in l]
-	return tags
+    l = nltk.pos_tag([word])
+    tags = [ t for (w,t) in l]
+    return tags
 
 def fillLists(words):
-	l=dict()
-	for w in words:
-		tags = getTags(w)
-		for t in tags:
-			if l.keys().count(t)==0:
-				l[t] = []
-			l[t].append(w)
-	return l
+    l=dict()
+    for w in words:
+        tags = getTags(w)
+        for t in tags:
+            if l.keys().count(t)==0:
+                l[t] = []
+            l[t].append(w)
+    return l
 
 def getRandomWordFromModifMask(modifMask,proverb):
-	nWordUnmodified = modifMask.count(False)
-	if nWordUnmodified==0:
-		return False
-	nWord = randint(0,nWordUnmodified)
-	index = 0
-	for i,m,w in zip(range(0,len(modifMask)),modifMask, proverb):
-		if m==False:
-			index = index + 1
-		if index==nWord:
-			return (i, w)
-	return (False, None)
+    nWordUnmodified = modifMask.count(False)
+    if nWordUnmodified==0:
+        return False
+    nWord = randint(0,nWordUnmodified)
+    index = 0
+    for i,m,w in zip(range(0,len(modifMask)),modifMask, proverb):
+        if m==False:
+            index = index + 1
+        if index==nWord:
+            return (i, w)
+    return (False, None)
 
 def pickUpWordFromEntitiesLists(entitiesList,form):
-	if entitiesList.keys().count(form)==0:
-		return (False, None)
-	else:
-		return (True, random.choice(entitiesList[form]))
+    if entitiesList.keys().count(form)==0:
+        return (False, None)
+    else:
+        return (True, random.choice(entitiesList[form]))
 
 def setWordInTheCorrectForm(word, form):
-	return (True,(word,form))
+    return (True,(word,form))
 
 def shuffle(x):
-	x = list(x)
-	random.shuffle(x)
-	return x
+    x = list(x)
+    random.shuffle(x)
+    return x
 
 def generateProverb(theme, rate=0.5):
-	# Load Background Corpus
-	backgroundGraph = nltk.text.ContextIndex([word.lower( ) for word in nltk.corpus.brown.words( )])
+    # Load Background Corpus
+    backgroundGraph = nltk.text.ContextIndex([word.lower( ) for word in nltk.corpus.brown.words( )])
 
-	# Build background graph from the theme
-	words = backgroundGraph.similar_words(theme,100)
-	entitiesList = fillLists(words)
+    # Build background graph from the theme
+    words = backgroundGraph.similar_words(theme,100)
+    entitiesList = fillLists(words)
 
-	# Get a proverb
-	proverb = getRandomProverb()
-	print(proverb)
-	lProverb = nltk.pos_tag(nltk.word_tokenize(proverb))
-	initialLProverb = lProverb
+    # Get a proverb
+    proverb = getRandomProverb()
+    print(proverb)
+    lProverb = nltk.pos_tag(nltk.word_tokenize(proverb))
+    initialLProverb = lProverb
 
-	modif = [False]*len(lProverb)
+    modif = [False]*len(lProverb)
 
-	# Main loop of the algorithm
+    # Main loop of the algorithm
 
-	permutation = shuffle(range(0,len(lProverb)))
-	for i in permutation:
-	
-		if computeModificationRate(initialLProverb,lProverb) > rate:
-			break
+    permutation = shuffle(range(0,len(lProverb)))
+    for i in permutation:
 
-		# Pick up a random word from the proverb
-		(index, word) = (i,lProverb[i])
+        if computeModificationRate(initialLProverb,lProverb) > rate:
+            break
 
+        # Pick up a random word from the proverb
+        (index, word) = (i,lProverb[i])
 
-		# Pick up a word from the background graph
-		(finded, newWord) = pickUpWordFromEntitiesLists(entitiesList,word[1])
+        # Pick up a word from the background graph
+        (finded, newWord) = pickUpWordFromEntitiesLists(entitiesList,word[1])
 
-		# Replace if the word can be put in the right form
-		if finded:
-			(isWordInCorrectForm, newWord) = setWordInTheCorrectForm(newWord, word[1])
-			if isWordInCorrectForm:
-				modif[index] = True
-				lProverb[index] = newWord
+        # Replace if the word can be put in the right form
+        if finded:
+            (isWordInCorrectForm, newWord) = setWordInTheCorrectForm(newWord, word[1])
+            if isWordInCorrectForm:
+                modif[index] = True
+                lProverb[index] = newWord
 
-		print(str(word) + " -> " + str(newWord))
+        print(str(word) + " -> " + str(newWord))
 
+    print(proverb)
 
-	print(proverb)
+    newProverb = ' '.join([w for (w,t) in lProverb])
+    print(newProverb)
 
-	newProverb = ' '.join([w for (w,t) in lProverb])
-	print(newProverb)
-
-	return newProverb
+    return newProverb
 
 
